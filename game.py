ff009d632957f2e7d857adad1ad36fe3327297db
Uchantal <u.chantal@alustudent.com>
2024-04-03 16:42:31 +0200

    added todo

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ select_number=9
 guess_count=0
 guess_limit=3
 
+# choose from random number 
+# provide limit like choose from 40-50
+
 while guess_count<guess_limit:
     guess=int(input('Guess: '))
     guess_count+=1
